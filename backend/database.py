c68f7e0f36aa0938973a39c719df7c6666b2f980
Corentin Seguin <corentin.seguin04@gmail.com>
2026-06-22 11:38:53 +0200

    Migration vers PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./mmotors.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./mmotors.db"
 
 engine = create_engine(
-    DATABASE_URL,
+    SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
